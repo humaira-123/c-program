763a4495cb2c52f6cd999fa89b917590293678d8
santosh <santosh@santoshyadavkq712.com>
2021-02-10 22:07:26 +0530

    enumeration

---
+++ enum.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+enum person{sit,stand,walk};
+void add(int a, int b)
+int main()
+{
+    person p;
+    p=sit;
+    cout<<p;
+    int a,b;
+    cin>>a,b;
+    add(a,b);
+}
+void add(int a,int b)
+{
+    int c;
+    c=a+b;
+    cout<<c;
+}
